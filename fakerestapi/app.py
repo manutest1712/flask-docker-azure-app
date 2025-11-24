2e25a07fcff9de87b6b9012854a9fe02a772ed35
manutest1712 <manugithub17121980@gmail.com>
2025-11-24 15:40:12 +0530

    Update app.py

--- fakerestapi/app.py
+++ fakerestapi/app.py
@@ -174,4 +174,4 @@ def delete_activity(activity_id):
 
 # ===== Run Flask App =====
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
